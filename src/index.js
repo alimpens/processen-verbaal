819f3c68f9b3439370ff77d848d962e7fa9b91d8
Aram Limpens <a.limpens@amsterdam.nl>
2021-03-08 16:15:04 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { registerHeightObserver } from 'element-height-observer'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,21 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// setup app to work well in iframe
+window.addEventListener('load', function () {
+  // add class to body if document is in iframe to remove scrollbar in css
+  if (window.top!==window.self) { document.body.className += " framed"}
+
+  const root = document.getElementById('root')
+
+  // send height to iframe onload
+  let message = 'documentHeight:'+root.scrollHeight
+  window.parent.postMessage(message,'*');
+
+  // send height to iframe when height of root changes
+  registerHeightObserver(root, { direction: 'vertical' }, function () {
+    let message = 'documentHeight:'+root.scrollHeight
+    window.parent.postMessage(message,'*');
+  })
+});
