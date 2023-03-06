52983fffcc578b7c3634e78258277bf380c55304
Aram Limpens <a.limpens@amsterdam.nl>
2023-03-06 11:10:13 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@ import { useState, useEffect } from 'react'
 import {
   GlobalStyle,
   ThemeProvider,
-  Row,
-  Column,
-  Heading,
   Paragraph,
   Link,
   Select,
   Button,
   themeSpacing,
   Spinner,
-  ErrorMessage
+  ErrorMessage,
 } from '@amsterdam/asc-ui'
 import { Download } from '@amsterdam/asc-assets'
 import styled from 'styled-components'
 import useDownload from './useDownload'
 
-const URL = 'https://api.data.amsterdam.nl/v1/verkiezingen/processenverbaal/?page_size=10000&verkiezingsjaar=2021'
+const urlFragment =
+  'https://api.data.amsterdam.nl/v1/verkiezingen/processenverbaal/?verkiezingsjaar=2023&page_size=10000&&documentnaam[like]='
 
 const zeroPad = (num, places) => String(num).padStart(places, '0')
-
-const StyledColumn = styled(Column)`
-  flex-direction: column;
-`
 
 const StyledForm = styled.form`
   width: 100%;
@@ -41,26 +35,56 @@ const SpinnerWrapper = styled.div`
 
 function App() {
   const [data, setData] = useState()
+  const [config, setConfig] = useState()
+  const [election, setElection] = useState('provinciale_staten')
   const [value, setValue] = useState()
   const [downloadLoading, downloadFile, error] = useDownload()
   const [resetError, setResetError] = useState(false)
   const [disableButton, setDisableButton] = useState(true)
 
   useEffect(() => {
-    let isMounted = true;
+    let isMounted = true
 
-    fetch(URL)
-      .then(response => response.json())
-      .then(json => {
+    fetch(`${urlFragment}*${election}*`)
+      .then((response) => response.json())
+      .then((json) => {
         if (isMounted) {
           setData(json)
         }
-      });
+      })
 
-    return () => { isMounted = false; }
+    return () => {
+      isMounted = false
+    }
+  }, [election])
+
+  useEffect(() => {
+    let isMounted = true
+
+    const rootDiv = document.querySelector('#root')
+    const config = rootDiv?.dataset?.config
+    const configUrl = config && JSON.parse(config)?.contentApi
+
+    fetch(configUrl)
+      .then((response) => response.json())
+      .then((json) => {
+        if (isMounted) {
+          setConfig(json)
+        }
+      })
+
+    return () => {
+      isMounted = false
+    }
   }, [])
 
-  const handleChange = e => {
+  const handleElectionChange = (e) => {
+    setElection(e.target.value)
+    setValue('placeholder')
+    setDisableButton(true)
+  }
+
+  const handleChange = (e) => {
     setValue(e.target.value)
     setResetError(true)
     setDisableButton(false)
@@ -74,53 +98,74 @@ function App() {
   return (
     <ThemeProvider>
       <GlobalStyle />
-      <Row>
-        <StyledColumn
-          wrap
-          span={{ small: 1, medium: 2, big: 4, large: 7, xLarge: 7 }}
-          push={{ small: 0, medium: 0, big: 1, large: 1, xLarge: 1 }}
+      <Paragraph>
+        Hier kunt u de processen-verbaal van de Amsterdamse stembureaus
+        downloaden.
+      </Paragraph>
+      <Paragraph>
+        {config && (
+          <Link
+            href={`${config?.applicatie['extra-velden']?.variabelen?.value}2022-03-16-alle-stembureaus-voor-uitslag.pdf`}
+            variant="inline"
+            target="_blank"
+          >
+            Bekijk hier de lijst met alle stembureaus
+          </Link>
+        )}
+      </Paragraph>
+      <StyledForm>
+        <StyledSelect
+          value={election}
+          onChange={handleElectionChange}
+          label={'Kies verkiezing'}
         >
-          <Heading gutterBottom={28}>Proces verbalen stembureaus Amsterdam Tweede Kamerverkiezingen 2021</Heading>
-          <Paragraph>Hier kunt u de proces verbalen van de Amsterdamse stembureaus downloaden.</Paragraph>
-          <Paragraph>
-            <Link href={`${process.env.REACT_APP_URL}/2021-03-15-alle-stembureaus-voor-uitslag.pdf`} variant='inline' target='_blank'>Bekijk hier de lijst met alle stembureaus</Link>
-          </Paragraph>
-          {data ?
-          <StyledForm>
+          <option value="provinciale_staten">
+            Provinciale Staten Noord-Holland
+          </option>
+          <option value="agv">Waterschap Amstel, Gooi en Vecht</option>
+          <option value="hn">Hoogheemraadschap Hollands Noorderkwartier</option>
+          <option value="rijnland">Hoogheemraadschap van Rijnland</option>
+        </StyledSelect>
+        {data ? (
+          <>
             <StyledSelect
               defaultValue={'placeholder'}
               value={value}
               onChange={handleChange}
               label={'Kies stembureau'}
             >
-              <option value='placeholder' disabled hidden>Maak een keuze</option>
-              {data._embedded?.processenverbaal?.sort((a,b) => a.volgnummer-b.volgnummer).map(({ id, volgnummer, uri }) =>
-                <option
-                  key={id}
-                  value={uri}
-                >
-                  {`Stembureau ${zeroPad(volgnummer, 3)}`}
-                </option>
-              )}
+              <option value="placeholder" disabled hidden>
+                Maak een keuze
+              </option>
+              {data._embedded?.processenverbaal
+                ?.sort((a, b) => a.volgnummer - b.volgnummer)
+                .map(({ id, volgnummer, uri }) => (
+                  <option key={id} value={uri}>
+                    {`Stembureau ${zeroPad(volgnummer, 3)}`}
+                  </option>
+                ))}
             </StyledSelect>
             <Button
-              type='button'
+              type="button"
               iconLeft={downloadLoading ? <Spinner /> : <Download />}
               onClick={handleClick}
-              variant='primary'
+              variant="primary"
               disabled={disableButton || downloadLoading}
             >
               Download
             </Button>
-            {error && !resetError && <ErrorMessage message="Deze download is niet beschikbaar" />}
-          </StyledForm> :
+            {error && !resetError && (
+              <ErrorMessage message="Deze download is niet beschikbaar" />
+            )}
+          </>
+        ) : (
           <SpinnerWrapper>
             <Spinner size={24} />
-          </SpinnerWrapper>}
-        </StyledColumn>
-      </Row>
+          </SpinnerWrapper>
+        )}
+      </StyledForm>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
